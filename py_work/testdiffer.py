6c24b59c4ab3569589500ea817ad96193da82a81
xiangfei2017 <xfeixzc@aliyun.com>
2021-08-24 09:07:47 +0800

    change testdiffer.py

--- py_work/testdiffer.py
+++ py_work/testdiffer.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
-import mathdiffer as df
+import mathdiffer as nt
 
 
-x=df.TN(2,'x')
-y=df.TN(3,'y')
+x=nt.TN(2,'x')
+y=nt.TN(3,'y')
 
-z=x*df.log(x)
+z=x*nt.log(x)
 g=z.grad(x)
 
+print('x=',x)
 print('z=',z)
 print('expression of z:\n',z.getexpstr())
 
 print('g=',g)
-print(g.getpostsort())
+#print(g.getpostsort())
 print('expression of g:\n',g.getexpstr())
+
+def func(x):
+    y=x*nt.sin(x)
+    return y
+
+df=nt.get_diff(func)
+yy=func(x)
+print('f(x)=',yy.getexpstr())
+print('df(x)=',df(x))
+print('df(x):',df(x).getexpstr())
+
